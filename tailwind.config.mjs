6abaa14f24a92bb0c9d9984e8308eadaf5d6f07f
Jamie Henson <jamie@jamiehenson.com>
2024-11-28 22:23:22 +0000

    feat: add archivo font

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,16 +1,21 @@
+import defaultTheme from "tailwindcss/defaultTheme";
+
 /** @type {import('tailwindcss').Config} */
 export default {
-    darkMode: ['class'],
-    content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-	theme: {
-    	extend: {
-    		borderRadius: {
-    			lg: 'var(--radius)',
-    			md: 'calc(var(--radius) - 2px)',
-    			sm: 'calc(var(--radius) - 4px)'
-    		},
-    		colors: {}
-    	}
+  darkMode: ["class"],
+  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  theme: {
+    extend: {
+      borderRadius: {
+        lg: "var(--radius)",
+        md: "calc(var(--radius) - 2px)",
+        sm: "calc(var(--radius) - 4px)",
+      },
+      colors: {},
+      fontFamily: {
+        sans: ["Archivo", ...defaultTheme.fontFamily.sans],
+      },
     },
-	plugins: [require("tailwindcss-animate")],
-}
+  },
+  plugins: [require("tailwindcss-animate")],
+};
