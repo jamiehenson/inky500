a86e60ff1e98d762edc7c5bd662bce06f5e1bf93
Jamie Henson <jamie@jamiehenson.com>
2025-03-20 20:07:38 +0000

    chore: upgrade astro to 5, tailwind to 4, add svelte 5

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,15 @@
 // @ts-check
 import { defineConfig } from "astro/config";
-
 import tailwind from "@astrojs/tailwind";
+import svelte from "@astrojs/svelte";
 
 // https://astro.build/config
 export default defineConfig({
   base: "inky500",
-  integrations: [tailwind()],
+  integrations: [
+    tailwind({
+      applyBaseStyles: false,
+    }),
+    svelte(),
+  ],
 });
