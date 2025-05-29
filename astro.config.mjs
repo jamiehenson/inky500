9230984654dfd101d6a75b758a887aba9fae5e6c
Jamie Henson <jamie@jamiehenson.com>
2025-05-29 20:12:10 +0100

    chore: remove astro tw 3

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,15 +1,9 @@
 // @ts-check
 import { defineConfig } from "astro/config";
-import tailwind from "@astrojs/tailwind";
 import svelte from "@astrojs/svelte";
 
 // https://astro.build/config
 export default defineConfig({
   base: "inky500",
-  integrations: [
-    tailwind({
-      applyBaseStyles: false,
-    }),
-    svelte(),
-  ],
+  integrations: [svelte()],
 });
